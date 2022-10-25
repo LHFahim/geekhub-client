cbf8da7b240cc7c9f0f8b1aebb1a1eba06543e9f
Fahim-H <fahimlutful@gmail.com>
2022-10-25 12:48:56 +0600

    hide firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHNgyEBMqoZmTCh4pRhTkqrk2kKGAHkWY",
-  authDomain: "geekhub-a9522.firebaseapp.com",
-  projectId: "geekhub-a9522",
-  storageBucket: "geekhub-a9522.appspot.com",
-  messagingSenderId: "825366894434",
-  appId: "1:825366894434:web:74b9ad4ca5afc7d1467e3b",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: 'geekhub-a9522.firebaseapp.com',
+  projectId: 'geekhub-a9522',
+  storageBucket: 'geekhub-a9522.appspot.com',
+  messagingSenderId: '825366894434',
+  appId: '1:825366894434:web:74b9ad4ca5afc7d1467e3b',
 };
 
 // Initialize Firebase
