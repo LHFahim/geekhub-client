3390269ad8c078f33e3077c3c931527fd8f53316
Fahim-H <fahimlutful@gmail.com>
2022-10-25 16:18:58 +0600

    added react to pdf

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaDownload } from 'react-icons/fa';
-// import Pdf from 'react-to-pdf';
+import Pdf from 'react-to-pdf';
+import ReactToPdf from 'react-to-pdf';
 
 const CourseDetails = () => {
   const courseData = useLoaderData();
@@ -9,30 +10,40 @@ const CourseDetails = () => {
 
   // react to pdf
   const ref = React.createRef();
+  const options = {
+    orientation: 'landscape',
+  };
 
   return (
-    <div ref={ref} className="w-3/4 mx-auto">
-      <h1 className="text-3xl font-bold my-10 text-center">Course details</h1>
-      <div className="flex justify-center">
-        <img src={img} alt="" />
-      </div>
-      <div className="space-y-5 my-5 w-10/12 mx-auto px-10 py-5 border border-indigo-600">
-        <h3 className="text-2xl font-bold">
-          {' '}
-          {name}{' '}
-          <button>
-            <FaDownload className="inline" />
+    <div>
+      <div ref={ref} className="w-3/4 mx-auto">
+        <h1 className="text-3xl font-bold my-10 text-center">Course details</h1>
+        <div className="flex justify-center">
+          <img src={img} alt="" />
+        </div>
+        <div className="space-y-5 my-5 w-10/12 mx-auto px-10 py-5 border border-indigo-600">
+          <h3 className="text-2xl font-bold">
+            {' '}
+            {name}{' '}
+            <ReactToPdf
+              targetRef={ref}
+              filename="Course-details.pdf"
+              options={options}
+            >
+              {({ toPdf }) => (
+                <button onClick={toPdf}>
+                  <FaDownload className="inline" />
+                </button>
+              )}
+            </ReactToPdf>{' '}
+          </h3>
+
+          <p>{description}</p>
+
+          <button className="py-3 px-5 rounded text-white bg-indigo-700">
+            <Link to={`/checkout/${id}`}> Get premium access!</Link>
           </button>
-        </h3>
-        {/* <Pdf targetRef={ref} filename="code-example.pdf">
-          {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-        </Pdf> */}
-
-        <p>{description}</p>
-
-        <button className="py-3 px-5 rounded text-white bg-indigo-700">
-          <Link to={`/checkout/${id}`}> Get premium access!</Link>
-        </button>
+        </div>
       </div>
     </div>
   );
