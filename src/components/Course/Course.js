06eb27c9a90a04621a5d55943b2dc3eea870d89e
Fahim-H <fahimlutful@gmail.com>
2022-10-25 22:10:18 +0600

    fixed issues

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Course = ({ course }) => {
   const { id, name, img, description } = course;
@@ -13,6 +14,9 @@ const Course = ({ course }) => {
       <div className="space-y-5 mt-2">
         <h3 className="text-2xl font-bold"> {name} </h3>
         <p>{description.substring(0, 150)}...</p>
+        <Link className="text-indigo-600 font-semibold" to={`/course/${id}`}>
+          Click to read more
+        </Link>
       </div>
     </div>
   );
